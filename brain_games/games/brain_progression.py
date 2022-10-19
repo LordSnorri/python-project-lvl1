7b5adcb4945b745f3e34b5d83fd54440355ed4be
Vitalii Arzamastcev <vitaliebo@gmail.com>
2022-10-19 18:24:51 +0700

    Changed code according to flake8 brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,6 +4,7 @@ import random
 
 player_name = brain_games.cli.welcome_user()
 
+
 def main():
 
     win_count = 0
@@ -12,41 +13,37 @@ def main():
 
     while win_count < 3:
 
-        random_initial = random.randint(1,10)
-        
-        random_prog = random.randint(1,5)
-        
+        random_initial = random.randint(1, 10)
+        random_prog = random.randint(1, 5)
+
         new_list = [random_initial]
-        
+
         _count = 0
 
         while _count < 9:
             prev_num = new_list[_count]
             new_list.append(prev_num + random_prog)
             _count += 1
-        
-        random_number = random.choice(new_list)
-        random_number_index = new_list.index(random_number)
 
-        q_list = new_list.copy()
-        new_q_list = q_list.insert(random_number_index, '..')
-        new_q_list = q_list.remove(random_number)
-       
+        r_num = random.choice(new_list)
+        r_num_index = new_list.index(r_num)
 
-        print("Question: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(q_list[0],q_list[1],q_list[2],q_list[3],q_list[4],q_list[5],q_list[6],q_list[7],q_list[8],q_list[9]))
+        q_l = new_list.copy()
+        q_l.insert(r_num_index, '..')
+        q_l.remove(r_num)
 
-        user_input = input()
+        print("Question: ", end='')
+        print(*q_l, end='\n')
 
-        if user_input == str(random_number):
-            print("Your answer is : {}\nCorrect!".format(user_input))
+        u_key = input()
+
+        if u_key == str(r_num):
+            print("Your answer is : {}\nCorrect!".format(u_key))
             win_count += 1
-        elif user_input != str(random_number):
-            print("Your answer is : {}\n{} is wrong answer. Correct answer was {}\nLet's try again, {}!".format(user_input,user_input,random_number,player_name))
+        elif u_key != str(r_num):
+            print(f"Your answer is : {u_key}")
+            print(f"{u_key} is wrong answer. Correct answer was {r_num}")
+            print(f"Let's try again, {player_name}!")
             quit()
 
     print(f"Congratulations, {player_name}!")
-
-    
-
-
-
