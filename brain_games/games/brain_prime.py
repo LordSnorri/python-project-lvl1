354907cbfb0fc6ff830f8239a2622afdb793e4ee
Vitalii Arzamastcev <vitaliebo@gmail.com>
2022-10-19 17:33:23 +0700

    Changed script according to flake8 brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,6 +4,16 @@ import random
 player_name = brain_games.cli.welcome_user()
 
 
+def prime_check(number):
+    flag = 0
+    if number > 1:
+        for i in range(2, int(number / 2) + 1):
+            if (number % i) == 0:
+                flag = 1
+                return flag
+        return flag
+
+
 def main():
 
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
@@ -12,33 +22,25 @@ def main():
 
     while win_count < 3:
 
-        flag = 0
         random_number = random.randint(2, 100)
-        
+
         print('Question: ' + str(random_number))
 
         user_input = input()
 
-        if random_number > 1:
-            for i in range(2, int(random_number/2)+1):
-                if (random_number % i) == 0:
-                    flag = 1
-                    break
-            
-            if flag == 0 and user_input == 'yes':
-                print('Correct')
-                win_count += 1
-            elif flag == 0 and user_input == 'no':
-                print(f"'no' is wrong answer. Correct answer was 'yes'\nLet's try again, {player_name}!")
-                quit()
-            elif flag == 1 and user_input == 'no':
-                print('Correct')
-                win_count += 1
-            elif flag == 1 and user_input == 'yes':
-                print(f"'yes' is wrong answer. Correct answer was 'no'\nLet's try again, {player_name}!")
-                quit()
-            
+        if prime_check(random_number) == 0 and user_input == 'yes':
+            print('Correct!')
+            win_count += 1
+        elif prime_check(random_number) == 0 and user_input == 'no':
+            print("'no' is wrong answer. Correct answer was 'yes'.")
+            print(f"Let's try again, {player_name}!")
+            quit()
+        elif prime_check(random_number) == 1 and user_input == 'no':
+            print('Correct!')
+            win_count += 1
+        elif prime_check(random_number) == 1 and user_input == 'yes':
+            print("'yes' is wrong answer. Correct answer was 'no'.")
+            print(f"Let's try again, {player_name}!")
+            quit()
+
     print('Congratulations, ' + str(player_name) + '!')
-
-
-
